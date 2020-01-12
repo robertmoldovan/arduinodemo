16cfae144054a380c17ff9701c564dcd006c528c
Robert Claudiu Moldovan Duse <robert.moldovan.2004@gmail.com>
2020-01-12 14:13:05 +0200

    upload to arduiono nano

--- arduinodemo/arduinodemo/arduinodemo.ino
+++ arduinodemo/arduinodemo/arduinodemo.ino
@@ -4,12 +4,45 @@
  Author:	rober
 */
 
+const int ledPin = 13;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+    Serial.begin(9600);
+    pinMode(ledPin, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+    static int delayPeriod = 100; // 100 milisecconds
+    static int countDir = 1;
+
+    digitalWrite(ledPin, HIGH);
+    delay(delayPeriod);
+    digitalWrite(ledPin, LOW);
+    delay(delayPeriod);
+
+    countDir = checkDirChange(delayPeriod, countDir);
+
+    delayPeriod += 100 * countDir;
+    Serial.print("New Wait Time: ");
+    Serial.println(delayPeriod);
+}
+
+int checkDirChange(int delayPeriod, int countDir)
+{
+    if ((delayPeriod == 1000) || (delayPeriod == 0))
+    {
+        countDir *= -1;
+
+        if (countDir < 0) {
+            Serial.println("Going Down");
+        }
+        else
+        {
+            Serial.println("Going Up");
+        }
+    }
+
+    return countDir;
 }
