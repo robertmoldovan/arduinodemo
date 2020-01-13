13fdcd32af16fdab70db697fb260fb899f9abece
Robert Claudiu Moldovan Duse <robert.moldovan.2004@gmail.com>
2020-01-13 18:47:49 +0200

    laser pin mode

--- arduinodemo/laserdemo/laserdemo.ino
+++ arduinodemo/laserdemo/laserdemo.ino
@@ -4,20 +4,15 @@
  Author:	robert
 */
 
-const int ledPin = 10;
+const int laserPin = 6;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
     Serial.begin(9600);
-    pinMode(ledPin, OUTPUT);
+    pinMode(laserPin, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-    static int delayPeriod = 1000; // 100 milisecconds
-
-    digitalWrite(ledPin, HIGH);
-    delay(delayPeriod);
-    digitalWrite(ledPin, LOW);
-    delay(delayPeriod);
+    digitalWrite(laserPin, HIGH);
 }
