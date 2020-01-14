89e79b791642e467209b0a952c9549791f19002c
Robert Claudiu Moldovan Duse <robert.moldovan.2004@gmail.com>
2020-01-14 18:00:40 +0200

    add new proiect

---
+++ arduinodemo/temperature_sensor/temperature_sensor.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		temperature_sensor.ino
+ Created:	1/14/2020 5:59:57 PM
+ Author:	rober
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
