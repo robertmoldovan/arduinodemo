fa5a36e97836d5410e746688032146e51396e207
Robert Claudiu Moldovan Duse <robert.moldovan.2004@gmail.com>
2020-01-14 17:49:48 +0200

    add messages

--- arduinodemo/led_blinks/led_blinks.ino
+++ arduinodemo/led_blinks/led_blinks.ino
@@ -8,14 +8,17 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+    // initialize digital pin 13 as an output.
+    Serial.println("Start setup");
+    pinMode(13, OUTPUT);
+    Serial.println("End setup");
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+    Serial.println("Blink...");
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);              // wait for a second
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);              // wait for a second
 }
