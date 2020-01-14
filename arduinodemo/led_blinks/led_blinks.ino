9f7a499f806ce7df8d45ab9cf94e68ba91aa60d3
Robert Claudiu Moldovan Duse <robert.moldovan.2004@gmail.com>
2020-01-14 17:51:30 +0200

    set  ledgreen

--- arduinodemo/led_blinks/led_blinks.ino
+++ arduinodemo/led_blinks/led_blinks.ino
@@ -4,21 +4,28 @@
  Author:	rober
 */
 
- 
+const int ledPinRed = 13;
+const int ledPinGreen = 14;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
     // initialize digital pin 13 as an output.
     Serial.println("Start setup");
-    pinMode(13, OUTPUT);
+    pinMode(ledPinRed, OUTPUT);
+    pinMode(ledPinGreen, OUTPUT);
     Serial.println("End setup");
 }
 
 // the loop function runs over and over again forever
 void loop() {
     Serial.println("Blink...");
-    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(ledPinRed, HIGH);   // turn the LED on (HIGH is the voltage level)
     delay(1000);              // wait for a second
-    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(ledPinRed, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);              // wait for a second
+
+    digitalWrite(ledPinGreen, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);              // wait for a second
+    digitalWrite(ledPinGreen, LOW);    // turn the LED off by making the voltage LOW
     delay(1000);              // wait for a second
 }
